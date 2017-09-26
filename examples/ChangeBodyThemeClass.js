4a1be704cb23bdb70ee1fb90378e02dd60e0ce11
Антон Киршанов <maetchkin@gmail.com>
2017-09-26 13:18:05 +0300

    nah

--- examples/ChangeBodyThemeClass.js
+++
@@ -1,8 +0,0 @@
-export default function ChangeBodyThemeClass(theme) {
-    document.body.className =
-        Array
-            .from   ( document.body.classList               )
-            .filter ( classes => !~classes.indexOf('theme') )
-            .concat ( [`theme-${ theme }`]                  )
-            .join   ( ' ' )
-}
